afd7245397361c8171d69122b1bb23e37ddff742
Winnie Tam <wtwinnietam@gmail.com>
2018-10-13 14:22:31 -0400

    added address

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import TabBar from './components/TabBar';
-import Map from './components/Map';
+//import Map from './components/Map';
+import Address from './components/Address';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
-        <Map/>
+        <Address/>
         <TabBar/>
       </div>
     );
