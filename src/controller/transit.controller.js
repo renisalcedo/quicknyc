646cac58e265706304432771001dfe4da91ab3ee
renisalcedo <salcedoreynaldo5@gmail.com>
2018-10-13 12:39:58 -0400

    Modified key mapping

--- src/controller/transit.controller.js
+++ src/controller/transit.controller.js
@@ -4,16 +4,34 @@ const mta = require('./mta.controller')
 const distanceKey = process.env.DISTANCE_KEY
 
 class Transit {
-    getTime(unixtime) {
-        const date = new Date(unix_timestamp*1000)
-        const hours = date.getHours()
-        const minutes = "0" + date.getMinutes
+    async getDuration(req, res) {
+        const { train, station } = req.body
+        const trainData = await mta.filter(train)
+        res.status(200).send(trainData)
 
-        return `${hours}:${minutes.substr(-2)}`
+        //this.getDurationTime()
     }
 
-    async getDuration(to, from) {
+    getTime(unixtime) {
+        const date = new Date(unixtime*1000)
+        const hours = date.getHours()
+        let minutes = date.getMinutes()
+        if(String(minutes).length < 2) {
+            minutes = "0" + minutes
+        }
+
+        return `${hours}:${minutes}`
+    }
+    
+    async getDurationTime(to, from) {
         const apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+        to += ",NY"
+        from += ",NY"
         const distanceUrl = `${apiUrl}origins=${from}&destinations=${to}&transit_mode=subway&language=en-EN&key=${distanceKey}`
+
+        const data = await axios.get(distanceUrl)
+        return data.rows.elements[0].duration.text
     }
 }
+
+module.exports = new Transit()
